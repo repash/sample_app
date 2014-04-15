ae9d9769ee457d70999127075fa0d61481b469c6
Viktor Schmidt <schmidt.viktor@gmail.com>
2014-04-15 16:00:02 +0200

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
   # equal to -> validates(:name, presence: true)
@@ -10,4 +11,21 @@ class User < ActiveRecord::Base
   # Sichert nur Einzigartigkeit innerhalb der Applikation, nicht auf der DB !!! (mit Migration lösen)
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  # Erstellt einen 16 Zeichen langen zufälligen Token
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Hasht den Token
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    # Speichert den gehashten Token für den User in der DB
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
